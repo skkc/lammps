593f585cc048b64309a5de20b58e0f1b01ecd6f9
S.Kikuchi <skkt.9x18@keio.jp>
2021-05-31 19:45:50 +0900

    .

---
+++ build/styles/lmpgitversion.h
@@ -0,0 +1,8 @@
+#ifndef LMP_GIT_VERSION_H
+#define LMP_GIT_VERSION_H
+const bool LAMMPS_NS::LAMMPS::has_git_info = true;
+const char LAMMPS_NS::LAMMPS::git_commit[] = "6f00e98945efe690d1aa59ea446556f970f6cfa7";
+const char LAMMPS_NS::LAMMPS::git_branch[] = "master";
+const char LAMMPS_NS::LAMMPS::git_descriptor[] = "patch_27May2021";
+#endif
+
---
+++ build/styles/packages_nstencil.h
@@ -0,0 +1,6 @@
+#undef PACKAGE
+#define PACKAGE "USER-DPD"
+#include "USER-DPD/nstencil_half_bin_2d_ssa.h"
+#undef PACKAGE
+#define PACKAGE "USER-DPD"
+#include "USER-DPD/nstencil_half_bin_3d_ssa.h"
